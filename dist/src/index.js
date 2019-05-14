834f8a103d1aa1e8e9f18abc9c41960a33fabc71
umutHatipoglu <umut.hatipoglu@gmail.com>
2019-05-14 19:12:20 +0200

    Initial dist subtree commit

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,15 +1,32 @@
-import { Observable } from 'rxjs';
-var observable = Observable.create((observer) => {
-    observer.next('Hello World');
-    observer.next('Hello Again!');
-    observer.complete();
-    observer.next('Bye');
-});
-observable.subscribe((x) => logItem(x), (error) => logItem('Error: ' + error), () => logItem('Completed'));
-function logItem(val) {
-    var node = document.createElement('li');
-    var textnode = document.createTextNode(val);
-    node.appendChild(textnode);
-    document.getElementById("list").appendChild(node);
+/*
+ * Create a list that holds all of your cards
+ */
+/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
 }
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */ 
 //# sourceMappingURL=index.js.map
